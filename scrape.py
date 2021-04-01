ae0b3e9f662f72327246156c347c029e3407f215
PeaWarrior <jacksonchen171@gmail.com>
2021-03-31 22:14:50 -0400

    initiate get_nutrition_facts

--- scrape.py
+++ scrape.py
@@ -28,10 +28,37 @@ def get_data(link):
   except:
     print('Something went wrong')
 
+def get_nutrition_facts(smartLabel):
+  nutrition_facts = {'primary': {}, 'secondary': {}}
+  servings_per_container = smartLabel.find('p', {'class': 'nutrition-facts__servings'}).text
+  serving_size = smartLabel.find('p', {'class': 'nutrition-facts__serving-size'}).text
+  serving_size = ' '.join(serving_size.split())
+
+  table = smartLabel.select('tbody > tr')
+  
+  for i in range(2, len(table)):
+    if table[i].find('th'):
+      header = table[i].find('th').text
+      header = ' '.join(header.split())
+      if table[i].has_key('class'):
+        # should add for amount and dv
+        nutrition_facts['secondary'][header] = table[i].find('td').text
+      else:
+        #should add for amount and dv
+        nutrition_facts['primary'][header] = table[i].find('td').text
+        pass
+      print(nutrition_facts)
+      print('----------')
+
+  # print(servings_per_container)
+  # print(serving_size)
+  # print(table)
+
 for a in cereal_links:
   link = a['href']
   data = get_data(link)
 
+  name = data.find('h1').text
   details = data.find('p', {'itemprop': 'Product Description'}).text
   img = f"https:{data.find('img', {'itemprop': 'Product Image'})['src']}"
 
@@ -39,13 +66,15 @@ for a in cereal_links:
   smartLabel_data = get_data(smartLabel_link)
   
   ingredients = get_ingredients(smartLabel_data)
+  nutrition_facts = get_nutrition_facts(smartLabel_data)
 
-  cereal = {}
-  cereal['details'] = details
-  cereal['img'] = img
+  # cereal = {}
+  # cereal['details'] = details
+  # cereal['img'] = img
+  # cereal['ingredients'] = ingredients
 
-  cereals.append(cereal)
+  # cereals.append(cereal)
 
-print(cereals)
+# print(cereals)
 
 source.close()
