78b68d320a8b35e340445702f33ae59efd00f59c
PeaWarrior <jacksonchen171@gmail.com>
2021-04-01 14:15:11 -0400

    finish get_nutrition_facts

--- scrape.py
+++ scrape.py
@@ -29,37 +29,66 @@ def get_data(link):
     print('Something went wrong')
 
 def get_nutrition_facts(smartLabel):
-  nutrition_facts = {'primary': {}, 'secondary': {}}
   servings_per_container = smartLabel.find('p', {'class': 'nutrition-facts__servings'}).text
+  servings_per_container = ' '.join(servings_per_container.split()[3:])
   serving_size = smartLabel.find('p', {'class': 'nutrition-facts__serving-size'}).text
-  serving_size = ' '.join(serving_size.split())
+  serving_size = ' '.join(serving_size.split()[2:])
 
   table = smartLabel.select('tbody > tr')
+  calories = table[0].find_all('th')[1].text
+  primary = []
+  secondary = []
   
   for i in range(2, len(table)):
     if table[i].find('th'):
-      header = table[i].find('th').text
-      header = ' '.join(header.split())
-      if table[i].has_key('class'):
-        # should add for amount and dv
-        nutrition_facts['secondary'][header] = table[i].find('td').text
-      else:
-        #should add for amount and dv
-        nutrition_facts['primary'][header] = table[i].find('td').text
-        pass
-      print(nutrition_facts)
-      print('----------')
+      header = table[i].find('th')
+      name = table[i].find('th').text
+      name = ' '.join(name.split())
 
-  # print(servings_per_container)
-  # print(serving_size)
-  # print(table)
+      values = table[i].find_all('td')
+      amount = values[0].text
+      dv = values[1].text
+
+      nutrient = {
+        name: {
+          'amount': amount,
+          'dv': dv,
+        }
+      }
+
+      if header.has_attr('class'):
+        nutrient_type = header['class'][0]
+        prev_nutrient = primary[-1]
+
+        if nutrient_type == 'nutrition-facts__secondary-item':
+          secondary.append(nutrient)
+        elif nutrient_type == 'nutrition-facts__item--subfield1':
+          if 'subfields' not in prev_nutrient:
+            prev_nutrient['subfields'] = []
+          prev_nutrient['subfields'].append(nutrient)
+        elif nutrient_type == 'nutrition-facts__item--subfield2':
+          if 'subfields2' not in prev_nutrient['subfields'][-1]:
+            prev_nutrient['subfields'][-1]['subfields2'] = []
+          prev_nutrient['subfields'][-1]['subfields2'].append(nutrient)
+      else:
+        primary.append(nutrient)
+
+  nutrition_facts = {
+    'servings_per_container': servings_per_container,
+    'serving_size': serving_size,
+    'calories': calories,
+    'primary': primary,
+    'secondary': secondary,
+  }
+
+  return nutrition_facts
 
 for a in cereal_links:
   link = a['href']
   data = get_data(link)
 
-  name = data.find('h1').text
-  details = data.find('p', {'itemprop': 'Product Description'}).text
+  name = data.find('h1').text.strip()
+  details = data.find('p', {'itemprop': 'Product Description'}).text.strip()
   img = f"https:{data.find('img', {'itemprop': 'Product Image'})['src']}"
 
   smartLabel_link = data.find('span', {'class': 'smtLabelbtn'}).a['href']
@@ -68,13 +97,13 @@ for a in cereal_links:
   ingredients = get_ingredients(smartLabel_data)
   nutrition_facts = get_nutrition_facts(smartLabel_data)
 
-  # cereal = {}
-  # cereal['details'] = details
-  # cereal['img'] = img
-  # cereal['ingredients'] = ingredients
+  cereal = {
+    'name': name,
+    'details': details,
+    'img': img,
+    'ingredients': ingredients
+  }
+  cereals.append(cereal)
 
-  # cereals.append(cereal)
-
-# print(cereals)
 
 source.close()
